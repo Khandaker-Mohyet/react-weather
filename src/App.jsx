0f01dd34c9b165966aa966ca156def72ed8e81d0
Khandaker-Mohyet <mohyet008@gmail.com>
2026-09-11 01:03:32 +0600

    start working and routing

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+
+function App() {
+
+  return (
+    <div className="text-4xl text-center text-green-700">This is Khandaker Mohyet</div>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+  {
+    path: "/about",
+    element: <div>This is about page</div>,
+  },
+]);
+
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <RouterProvider router={router} />,
+);
